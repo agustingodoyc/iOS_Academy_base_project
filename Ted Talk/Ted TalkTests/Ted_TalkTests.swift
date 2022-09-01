724c550f85c1b1cda9195451471482c356aaf8bc
Agustín Godoy <a.godoy@globant.com>
2022-09-01 17:26:40 -0300

    Feature 3

--- Ted Talk/Ted TalkTests/Ted_TalkTests.swift
+++ Ted Talk/Ted TalkTests/Ted_TalkTests.swift
@@ -10,19 +10,20 @@ import XCTest
 
 class Ted_TalkTests: XCTestCase {
     
-    var sut: Parser!
-
+    // MARK: - Test init
+    
     override func setUpWithError() throws {
         try super.setUpWithError()
-        sut = Parser()
     }
 
     override func tearDownWithError() throws {
-        sut = nil
         try super.tearDownWithError()
     }
+    
+    // MARK: - Test parse
 
     func testParse() {
+        let sut: Parser = Parser()
         let promise = self.expectation(description: "Scaling")
         var talks: [TedTalk]?
 
@@ -45,6 +46,7 @@ class Ted_TalkTests: XCTestCase {
     }
 
     func  testParseToFail() {
+        let sut: Parser = Parser()
         let promise = self.expectation(description: "Scaling")
 
         sut.parse("xxxx") { result in
@@ -57,5 +59,24 @@ class Ted_TalkTests: XCTestCase {
         }
         
         waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    // MARK: - Test searchWord
+    
+    func testSearchWord() {
+        let sut: DataManager = DataManager("test")
+        let promise = self.expectation(description: "Scaling")
+        var talks: [TedTalk]?
+        
+        sut.getTalks { result in
+            talks = result
+            promise.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        talks = sut.serchWord(searchText: "x", picker: "description")
+        
+        XCTAssertEqual(talks?.count, 0, "Score computed from guess is wrong")
     }
 }
